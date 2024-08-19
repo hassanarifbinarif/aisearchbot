eb4a5e0c066a72f5024c51150840b54293317a1e
hassanarif2811@gmail.com <hassanarif2811@gmail.com>
2024-08-19 17:40:02 +0500

    Fixed issue due to unneccessary url.

--- asb/urls.py
+++ asb/urls.py
@@ -61,5 +61,5 @@ urlpatterns = [
     path('remove-record-from-list/', views.remove_record_from_list, name='remove_record_from_list'),
     path('remove-candidate-from-list/', views.remove_candidate_from_list, name='remove_candidate_from_list'),
     path('get-list-candidates/<int:pk>', views.get_list_candidates, name='get_list_candidates'),
-    path('grouped-profiles-view/', views.grouped_profiles_view, name='grouped_profiles_view')
+    # path('grouped-profiles-view/', views.grouped_profiles_view, name='grouped_profiles_view')
 ]
